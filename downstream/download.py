4155d411ed6e533bd4fae9a8510ad9c42b929666
Patrick <synapse.and.syntax@gmail.com>
2011-12-15 21:10:13 -0500

    more logging

--- downstream/download.py
+++ downstream/download.py
@@ -10,9 +10,9 @@ def log(text):
         logfile.write(text + "\n")
 
 Entrez.email = "pon2@umbc.edu"
-print os.getcwd()
+log( os.getcwd())
 log("starting at" + time.asctime(time.localtime()))
-print "parsing upstream regions"
+log( "parsing upstream regions")
 if not os.path.isfile("upstream_regions.pkl"):
     upstream_regions = parse_urs("../upstream/upstream5000.fa")
     with open("../upstream/upstream_regions.pkl",'w') as ur_handle:
@@ -20,14 +20,14 @@ if not os.path.isfile("upstream_regions.pkl"):
 else:
     with open("../upstream/upstream_regions.pkl") as ur_handle:
         upstream_regions = pickle.load(ur_handle)
-print "computing mapping"
+log("computing mapping")
 mapping = open("gene_mrna_mapping.txt").readlines()
-print "computing refseqs"
+log("computing refseqs")
 refseqs = [filter(lambda x: x,line.strip().split('\t'))
            for line in mapping if line.strip().startswith('NM')]
-print "computing accession numbers"
+log("computing accession numbers")
 nms = [nm[:nm.index('_',3)] for nm in upstream_regions.keys()]
-print "finding relevant refseqs"
+log("finding relevant refseqs")
 if not os.path.isfile("desired_refseqs.pkl"):
     desired_refseqs = [rs for rs in refseqs if rs[0] in nms]
     with open("desired_refseqs.pkl",'w') as pickle_handle:# heh
@@ -39,62 +39,62 @@ wrong_lengths = []
 bad_apples = []
 retries = []
 genes = {}
-print "starting lookup"
+log("starting lookup")
 for dr in desired_refseqs:
     if len(dr) == 3:
         idnum = [entry for entry in dr
                  if (entry != '1' and entry != '-1'
                      and not entry.startswith('NM_'))][0]
-        print "looking up ", idnum
+        log("looking up ", idnum)
         handle = Entrez.esearch(db="nucleotide", term=idnum)
         record = Entrez.read(handle)
-        print "found", len(record['IdList']), "entries"
+        log("found", len(record['IdList']), "entries")
         genes[idnum] = {}
         for il in record['IdList']:
             if os.path.isfile(il + ".gbk"):
-                print "already looked up", il
+                log("already looked up", il)
                 with open(il + ".gbk") as in_handle:
                     try:
                         record = SeqIO.read(in_handle, "genbank")
                         genes[idnum][il] = record
                     except:
-                        print "record for",il, "was empty...bailing!"
+                        log("record for",il, "was empty...bailing!")
                         retries.append(il)
             else:
-                print "fetching", il
+                log("fetching", il)
                 net_handle = Entrez.efetch(db="nucleotide",id=il,rettype="gb")
                 try:
                     record = SeqIO.read(net_handle,"genbank")
                 except:
-                    print "couldn't read",il, "from net_handle...bailing!"
+                    log("couldn't read",il, "from net_handle...bailing!")
                     bad_apples.append(il)
                     continue
-                print "writing to disk"
+                log("writing to disk")
                 try:
                     with open(il + ".gbk",'w') as out_handle:
                         out_handle.write(record.format("gb"))
-                    print "storing in dictionary"
+                    log("storing in dictionary")
                     genes[idnum][il] = record
                 except:
-                    print "couldn't write", il, "to disk... bailing!"
+                    log("couldn't write", il, "to disk... bailing!")
                     bad_apples.append(record)
                     continue
     else:
         wrong_lengths.append(dr)
 
-print "Summary"
-print "_" * 80
-print "scanned", len(desired_refseqs), "refseqs"
-print "recovered", len([il for gene in genes for il in gene]), "genes"
-print len(wrong_lengths), "were of wrong length"
-print "encountered problems with", len(bad_apples)
-print "should retry", len(retries)
+log("Summary")
+log("_" * 80)
+log("scanned", len(desired_refseqs), "refseqs")
+log("recovered", len([il for gene in genes for il in gene]), "genes")
+log(len(wrong_lengths), "were of wrong length")
+log("encountered problems with", len(bad_apples))
+log("should retry", len(retries))
 
 if not os.path.isfile("genes.pkl"):
     with open("genes.pkl",'w') as gene_handle:# heh
         pickle.dump(genes,gene_handle)
 else:
-    print "warning: genes.pkl already exists"
-    print "saving to backup"
+    log("warning: genes.pkl already exists")
+    log("saving to backup")
     with open("genes.pkl.bak",'w') as gene_handle:# heh
         pickle.dump(genes,gene_handle)
