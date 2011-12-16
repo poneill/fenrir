c5fdef3e408cee5a2e270f6a484c73b47f1f9e0e
Patrick <synapse.and.syntax@gmail.com>
2011-12-15 20:27:25 -0500

    split the file up into manageable chunks while we're at it

--- upstream/get_upsteam5000.sh
+++ upstream/get_upsteam5000.sh
@@ -2,3 +2,6 @@
 #Get upstream5000.fa.  Clean this up later for public consumption.
 DIRECTORY=/mnt/kann2/ds2/data2/SHARE/transfac_data/October_2011/
 scp pon2@kann2.bioinf.umbc.edu:$DIRECTORY/upstream5000.fa .
+
+# split them up into chunks of 300 refseqs
+split -l 30300 upstream5000.fa
