6eb2a683bfc450c632466b87b2681720e8c771d1
Patrick <synapse.and.syntax@gmail.com>
2011-12-15 20:30:40 -0500

    add file extensions to chunkcs

--- upstream/get_upsteam5000.sh
+++ upstream/get_upsteam5000.sh
@@ -3,5 +3,6 @@
 DIRECTORY=/mnt/kann2/ds2/data2/SHARE/transfac_data/October_2011/
 scp pon2@kann2.bioinf.umbc.edu:$DIRECTORY/upstream5000.fa .
 
-# split them up into chunks of 300 refseqs
+# split them up into chunks of 300 refseqs and rename appropriately
 split -l 30300 upstream5000.fa
+for file in `ls x*`;do mv $file $file.fa;done
