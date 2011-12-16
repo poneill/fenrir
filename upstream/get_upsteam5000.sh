6a1aeb9323fb5ed5132aa3a704f0ffef227f50de
Patrick <synapse.and.syntax@gmail.com>
2011-12-15 20:21:56 -0500

    init commit

---
+++ upstream/get_upsteam5000.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Get upstream5000.fa.  Clean this up later for public consumption.
+DIRECTORY=/mnt/kann2/ds2/data2/SHARE/transfac_data/October_2011/
+scp pon2@kann2.bioinf.umbc.edu:$DIRECTORY/upstream5000.fa .
